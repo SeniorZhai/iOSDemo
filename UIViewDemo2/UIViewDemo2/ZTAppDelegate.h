bd9f563893c1304681f3382093413fcfb4378784
仔仔 <370985116@qq.com>
2014-02-22 19:42:22 +0800

    Demo2

---
+++ UIViewDemo2/UIViewDemo2/ZTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ZTAppDelegate.h
+//  UIViewDemo2
+//
+//  Created by 翟涛 on 14-2-22.
+//  Copyright (c) 2014年 翟涛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+@property (strong, nonatomic) UILabel *show;
+@end
